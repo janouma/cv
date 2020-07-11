faa575ec3cc4011b1b6256eaa7eceebd08519a54
Judicaël Anouma <janouma@gmail.com>
2020-07-11 18:37:52 +0200

    use "smooth" behavior to scroll elements into view

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-function revealPosition ({ currentTarget, currentTarget: { parentNode, checked } }) {
+function revealPosition ({ currentTarget }) {
+  const wrapper = currentTarget.closest('.position-wrapper')
   const { document, setTimeout, scrollBy } = window
   const multiColumnsThreshold = 540
   const isMultiColumns = document.documentElement.clientWidth >= multiColumnsThreshold
   const defaultMargin = 16
 
   currentTarget.focus()
-  parentNode.style.order = checked && isMultiColumns ? 0 : 1
+  wrapper.style.order = currentTarget.checked && isMultiColumns ? 0 : 1
 
-  setTimeout(() => {
-    parentNode.scrollIntoView()
-    scrollBy({ top: -defaultMargin, left: 0 })
-  }, 50)
+  setTimeout(() => wrapper.scrollIntoView({ behavior: 'smooth' }), 50)
 }
